ce851f4846690060df73904bfe987fc1de514211
leonardocampelo <66441050+leonardocampelo@users.noreply.github.com>
2020-06-04 19:27:11 -0300

    Create ClasseB.java

---
+++ ClasseB.java
@@ -0,0 +1,29 @@
+public class ClasseB {
+    private int B1;
+    private float B2;
+
+    public int getAB() {
+        return B1;
+    }
+
+    public float getA2() {
+        return B2;
+    }
+
+    public void setA1(int b1) {
+        B1 = b1;
+    }
+
+    public void setA2(float b2) {
+        B2 = b2;
+    }
+
+    public void MB1() {
+        System.out.println("MB1");
+    }
+
+    public void MB2() {
+        System.out.println("MB2");
+    }
+    
+}
