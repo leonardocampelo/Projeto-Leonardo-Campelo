9360ad09edc33b14287afaa9bea4c67be37d2f4a
Leonardo Campelo <leonardo.santos@estudante.ufla.br>
2020-06-04 20:53:48 -0300

    Criação da Classe D

---
+++ ClasseD.java
@@ -0,0 +1,29 @@
+public class ClasseD {
+    private int D1;
+    private String D2;
+
+    public int getD1() {
+        return D1;
+    }
+
+    public String getD2() {
+        return D2;
+    }
+
+    public void setD1(int d1) {
+        D1 = d1;
+    }
+
+    public void setD2(String d2) {
+        D2 = d2;
+    }
+
+    public void MD1() {
+        System.out.println("MD1");
+    }
+
+    public void MD2() {
+        System.out.println("MD2");
+    }
+
+}
